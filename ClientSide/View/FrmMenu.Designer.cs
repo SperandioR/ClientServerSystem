10264ab2c57276a6424458022348747b055a5fb7
SperandioR <rodrigopaulino13@gmail.com>
2023-10-13 20:25:16 -0300

    Esse código é usado para permitir que o usuário edite informações de um produto quando ele clica na célula "Edit" em uma linha específica do DataGridView "GridProducts". A edição é feita em um novo formulário chamado "FrmCadastro".

--- ClientSide/View/FrmMenu.Designer.cs
+++ ClientSide/View/FrmMenu.Designer.cs
@@ -36,16 +36,16 @@
             this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
             this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
             this.GridProducts = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.BtnNovo = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Edt = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Edit = new System.Windows.Forms.DataGridViewImageColumn();
             this.Del = new System.Windows.Forms.DataGridViewImageColumn();
-            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
-            this.BtnnNovo = new Guna.UI2.WinForms.Guna2Button();
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.pnCima.SuspendLayout();
             this.guna2GroupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.GridProducts)).BeginInit();
@@ -109,7 +109,7 @@
             this.Column3,
             this.Column4,
             this.Column5,
-            this.Edt,
+            this.Edit,
             this.Del});
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
@@ -147,6 +147,74 @@
             this.GridProducts.ThemeStyle.RowsStyle.Height = 22;
             this.GridProducts.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.GridProducts.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.GridProducts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridProducts_CellContentClick);
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(60)))), ((int)(((byte)(107)))));
+            this.guna2TextBox1.BorderRadius = 10;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(123)))), ((int)(((byte)(24)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2TextBox1.ForeColor = System.Drawing.Color.Black;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.IconLeft = ((System.Drawing.Image)(resources.GetObject("guna2TextBox1.IconLeft")));
+            this.guna2TextBox1.IconLeftSize = new System.Drawing.Size(30, 30);
+            this.guna2TextBox1.Location = new System.Drawing.Point(12, 75);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "Pesquisar Registro";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(303, 45);
+            this.guna2TextBox1.TabIndex = 15;
+            // 
+            // BtnNovo
+            // 
+            this.BtnNovo.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(123)))), ((int)(((byte)(24)))));
+            this.BtnNovo.BorderRadius = 20;
+            this.BtnNovo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnNovo.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.BtnNovo.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.BtnNovo.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.BtnNovo.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.BtnNovo.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(60)))), ((int)(((byte)(107)))));
+            this.BtnNovo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnNovo.ForeColor = System.Drawing.Color.White;
+            this.BtnNovo.Image = ((System.Drawing.Image)(resources.GetObject("BtnNovo.Image")));
+            this.BtnNovo.ImageSize = new System.Drawing.Size(40, 40);
+            this.BtnNovo.Location = new System.Drawing.Point(322, 75);
+            this.BtnNovo.Name = "BtnNovo";
+            this.BtnNovo.Size = new System.Drawing.Size(126, 44);
+            this.BtnNovo.TabIndex = 16;
+            this.BtnNovo.Text = "NOVO";
+            this.BtnNovo.Click += new System.EventHandler(this.BtnnNovo_Click);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(123)))), ((int)(((byte)(24)))));
+            this.guna2Button1.BorderRadius = 20;
+            this.guna2Button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(60)))), ((int)(((byte)(107)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.Image")));
+            this.guna2Button1.ImageSize = new System.Drawing.Size(40, 40);
+            this.guna2Button1.Location = new System.Drawing.Point(454, 75);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(126, 44);
+            this.guna2Button1.TabIndex = 17;
+            this.guna2Button1.Text = "PRINT";
             // 
             // Column1
             // 
@@ -187,15 +255,15 @@
             this.Column5.ReadOnly = true;
             this.Column5.Width = 79;
             // 
-            // Edt
+            // Edit
             // 
-            this.Edt.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.Edt.HeaderText = "Editar";
-            this.Edt.Image = ((System.Drawing.Image)(resources.GetObject("Edt.Image")));
-            this.Edt.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
-            this.Edt.Name = "Edt";
-            this.Edt.ReadOnly = true;
-            this.Edt.Width = 67;
+            this.Edit.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Edit.HeaderText = "Editar";
+            this.Edit.Image = ((System.Drawing.Image)(resources.GetObject("Edit.Image")));
+            this.Edit.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Edit.Name = "Edit";
+            this.Edit.ReadOnly = true;
+            this.Edit.Width = 67;
             // 
             // Del
             // 
@@ -207,73 +275,6 @@
             this.Del.ReadOnly = true;
             this.Del.Width = 84;
             // 
-            // guna2TextBox1
-            // 
-            this.guna2TextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(60)))), ((int)(((byte)(107)))));
-            this.guna2TextBox1.BorderRadius = 10;
-            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox1.DefaultText = "";
-            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(123)))), ((int)(((byte)(24)))));
-            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2TextBox1.ForeColor = System.Drawing.Color.Black;
-            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.IconLeft = ((System.Drawing.Image)(resources.GetObject("guna2TextBox1.IconLeft")));
-            this.guna2TextBox1.IconLeftSize = new System.Drawing.Size(30, 30);
-            this.guna2TextBox1.Location = new System.Drawing.Point(12, 75);
-            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.guna2TextBox1.Name = "guna2TextBox1";
-            this.guna2TextBox1.PasswordChar = '\0';
-            this.guna2TextBox1.PlaceholderText = "Pesquisar Registro";
-            this.guna2TextBox1.SelectedText = "";
-            this.guna2TextBox1.Size = new System.Drawing.Size(303, 45);
-            this.guna2TextBox1.TabIndex = 15;
-            // 
-            // BtnnNovo
-            // 
-            this.BtnnNovo.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(123)))), ((int)(((byte)(24)))));
-            this.BtnnNovo.BorderRadius = 20;
-            this.BtnnNovo.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.BtnnNovo.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.BtnnNovo.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.BtnnNovo.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.BtnnNovo.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.BtnnNovo.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(60)))), ((int)(((byte)(107)))));
-            this.BtnnNovo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnnNovo.ForeColor = System.Drawing.Color.White;
-            this.BtnnNovo.Image = ((System.Drawing.Image)(resources.GetObject("BtnnNovo.Image")));
-            this.BtnnNovo.ImageSize = new System.Drawing.Size(40, 40);
-            this.BtnnNovo.Location = new System.Drawing.Point(322, 75);
-            this.BtnnNovo.Name = "BtnnNovo";
-            this.BtnnNovo.Size = new System.Drawing.Size(126, 44);
-            this.BtnnNovo.TabIndex = 16;
-            this.BtnnNovo.Text = "NOVO";
-            this.BtnnNovo.Click += new System.EventHandler(this.BtnnNovo_Click);
-            // 
-            // guna2Button1
-            // 
-            this.guna2Button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(123)))), ((int)(((byte)(24)))));
-            this.guna2Button1.BorderRadius = 20;
-            this.guna2Button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(60)))), ((int)(((byte)(107)))));
-            this.guna2Button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.Image")));
-            this.guna2Button1.ImageSize = new System.Drawing.Size(40, 40);
-            this.guna2Button1.Location = new System.Drawing.Point(454, 75);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(126, 44);
-            this.guna2Button1.TabIndex = 17;
-            this.guna2Button1.Text = "PRINT";
-            // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
@@ -281,7 +282,7 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(38)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(1067, 588);
             this.Controls.Add(this.guna2Button1);
-            this.Controls.Add(this.BtnnNovo);
+            this.Controls.Add(this.BtnNovo);
             this.Controls.Add(this.guna2TextBox1);
             this.Controls.Add(this.guna2GroupBox1);
             this.Controls.Add(this.pnCima);
@@ -305,7 +306,7 @@
         private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
         private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
-        private Guna.UI2.WinForms.Guna2Button BtnnNovo;
+        private Guna.UI2.WinForms.Guna2Button BtnNovo;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private Guna.UI2.WinForms.Guna2DataGridView GridProducts;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
@@ -313,7 +314,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewImageColumn Edt;
+        private System.Windows.Forms.DataGridViewImageColumn Edit;
         private System.Windows.Forms.DataGridViewImageColumn Del;
     }
 }
--- ClientSide/View/FrmMenu.cs
+++ ClientSide/View/FrmMenu.cs
@@ -25,15 +25,31 @@ namespace ClientSide.View
         private void FrmMenu_Load(object sender, EventArgs e)
         {
             GridProducts.Rows.Add("1", "Peixe Porquinho", "34", "250.00", "08-10-2023");
-            GridProducts.Rows.Add("2", "Peixe Porquinho", "34", "250.00", "08-10-2023");
-            GridProducts.Rows.Add("3", "Peixe Porquinho", "34", "250.00", "08-10-2023");
-            GridProducts.Rows.Add("4", "Peixe Porquinho", "34", "250.00", "08-10-2023");
+            GridProducts.Rows.Add("2", "Banana", "72", "127.20", "08-10-2023");
+            GridProducts.Rows.Add("3", "Maçã", "120", "198.24", "08-10-2023");
+            GridProducts.Rows.Add("4", "Linguiça", "310", "540.00", "08-10-2023");
+            GridProducts.Rows.Add("5", "Picanha", "98", "720.30", "08-10-2023");
+            GridProducts.Rows.Add("6", "Pão de Alho", "236", "147.00", "08-10-2023");
+            GridProducts.Rows.Add("7", "Bolacha Bauducco", "34", "250.00", "08-10-2023");
+            GridProducts.Rows.Add("8", "Vinho Tinto", "287", "620.00", "08-10-2023");
+            GridProducts.Rows.Add("9", "Panetone", "144", "95.87", "08-10-2023");
+            GridProducts.Rows.Add("10", "Queijo Provolone", "68", "360.00", "08-10-2023");
         }
 
         private void BtnnNovo_Click(object sender, EventArgs e)
         {
             FrmCadastro frm = new FrmCadastro("Novo Produto");
             frm.ShowDialog();
+        }
+
+        private void GridProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string coluna = GridProducts.Columns[e.ColumnIndex].Name;
+            if(coluna == "Edit")
+            {
+                FrmCadastro frm = new FrmCadastro("Editar Produto");
+                frm.ShowDialog();
+            }
         }
     }
 }
